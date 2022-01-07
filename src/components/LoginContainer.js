7bfcdfeded751de45401595ace15a15641ce92ac
Guilherme mesquita rocha <guilherme.mesquita.rocha@gmail.com>
2022-01-06 22:19:24 -0300

    resolvendo alguns bugs

--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -21,7 +21,7 @@ export default ({onReceive})=>{
 		        <div class="text">
 			    <h2>Whatsapp <WhatsAppIcon className="icone"/></h2>
 			    <p>Com o WhatsApp para web e computador, você pode sincronizar facilmente suas conversas no seu computador e conversar da maneira que for mais conveniente para você.</p>
-			    <a class="btn" onClick={handleFacebookLogin}>Entrar</a>
+			    <button class="btn" onClick={handleFacebookLogin}>Entrar</button>
 		    </div>
 	    </div>
         <div className="imgBx">
@@ -29,6 +29,5 @@ export default ({onReceive})=>{
         </div>
     </div>
     )
-    
     }
     
